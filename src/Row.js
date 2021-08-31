7df8e04325e04bc0b18845946aeb06c1cac6981d
Igor Barale <svitaxiom@gmail.com>
2021-08-31 18:26:11 -0400

    init commit

---
+++ src/Row.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  // A snippet of code which runs on a specific condition/variable
+  // When the row loads, I want this to make a request to TMDB
+  useEffect(() => {
+    // if [], run once when the row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      // "https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213"
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {/* several row_posters */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
